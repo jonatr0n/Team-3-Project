8cd78e88a849e5fb0bb19481f2664ef63e49c3ed
Young Cho <youngtae1386@gmail.com>
2019-01-27 21:49:06 -0600

    adding firebasejs file

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -66,17 +66,4 @@ $(document).ready(function () {
 }); ``
 
 
-//Firebase connection 
-var config = {
-    apiKey: "AIzaSyCMZTfU_JsxH2_4e3WbjjEjagfOvNV3EHc",
-    authDomain: "team-3-project-1548374565657.firebaseapp.com",
-    databaseURL: "https://team-3-project-1548374565657.firebaseio.com",
-    projectId: "team-3-project-1548374565657",
-    storageBucket: "team-3-project-1548374565657.appspot.com",
-    messagingSenderId: "1073244498652"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-console.log(database);
 
